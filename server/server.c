b1bd2749af255c4b0235e35d120ec510c2a7b99f
kunalkushwaha <kunal.kushwaha@gmail.com>
2013-04-04 00:10:20 +0530

    updated comments in server.c

--- server/server.c
+++ server/server.c
@@ -1,6 +1,19 @@
+/*
+	Server starting point.
+	This server does following things.
+	
+	1. Intializes the server
+	2. Listen to client for connection.
+	3. Maintains list of clients in hash with info like IP, protocol supported etc
+	4. On cli requests, get information about client capablities.
+	5. can trigger the migration task.
+*/
+
 #include <stdio.h>
+
 
 int main()
 {
-	printf("Distributed Load Manager starting.........\n"); 
+	//
+
 }
